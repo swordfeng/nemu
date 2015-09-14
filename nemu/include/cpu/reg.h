e75cb1ee41b145104cd8be76dc65166f3e61d21f
swordfeng <swordfeng123@gmail.com>
2015-09-14 16:21:43 +0800

    re-organize register structure

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,19 +7,12 @@ enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
-/* TODO: Re-organize the `CPU_state' structure to match the register
- * encoding scheme in i386 instruction format. For example, if we
- * access cpu.gpr[3]._16, we will get the `bx' register; if we access
- * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
- * For more details about the register encoding scheme, see i386 manual.
- */
-
 typedef struct {
-	struct {
+	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
-	} gpr[8];
+	} gpr[0];
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
