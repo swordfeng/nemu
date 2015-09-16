e227d07acecf31cbd879e79b6ec26f72a67698ac
swordfeng <swordfeng123@gmail.com>
2015-09-16 17:46:33 +0800

    manage watchpoint pool

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -29,4 +29,23 @@ WP* new_wp() {
 	return wp;
 }
 
-void free_wp(WP *wp);
+void free_wp(WP *wp) {
+	// if wp is the first in the list
+	if (head == wp) {
+		head = wp->next;
+		wp->next = free_;
+		free_ = wp;
+		return;
+	} else {
+		WP *prev = head;
+		Assert(prev, "watchpoint is not active");
+		while (prev->next != wp) {
+			Assert(prev->next, "watchpoint is not active");
+			prev = prev->next;
+		}
+		prev->next = wp->next;
+		wp->next = free_;
+		free_ = wp;
+		return;
+	}
+}
