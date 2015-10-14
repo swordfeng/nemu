039e89db3d4ffa19e2c01c0ca26c1706acae5aec
swordfeng <swordfeng123@gmail.com>
2015-10-14 19:18:55 +0800

    fix asm instr suffix

--- nemu/src/cpu/data-mov/mov.h
+++ nemu/src/cpu/data-mov/mov.h
@@ -7,7 +7,8 @@ TEMPLATE_HELPER(mov) {
     int len = decode_operands<operand_names...>(ctx, eip);
     ctx.operands[0].setValue(ctx.operands[1].getValue());
     string instr_name = "mov";
-    if (ctx.operands[0].type == opt_address && ctx.operands[1].type == opt_address) {
+    if (ctx.operands[0].type == opt_address &&
+        (ctx.operands[1].type == opt_address || ctx.operands[1].type == opt_immediate)) {
         instr_name += ctx.operands[0].suffix();
     }
     print_instr(ctx, instr_name);
