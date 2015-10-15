452cdad156b9baa6464f56cac4d2f62108bb36e9
swordfeng <swordfeng123@gmail.com>
2015-10-15 09:59:31 +0800

    add eflags register

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,21 +7,50 @@ enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
-typedef union {
+typedef struct {
+	// GPRs
 	union {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
-
-	struct {
-		/* Do NOT change the order of the GPRs' definitions. */
-
-		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-		swaddr_t eip;
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
+		struct {
+			/* Do NOT change the order of the GPRs' definitions. */
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
 	};
-
+	// EFLAGS
+	struct {
+		union {
+			uint32_t eflags;
+			struct {
+				uint8_t cf : 1;
+				uint8_t __reserved1 : 1;
+				uint8_t pf : 1;
+				uint8_t __reserved3 : 1;
+				uint8_t af : 1;
+				uint8_t __reserved5 : 1;
+				uint8_t zf : 1;
+				uint8_t sf : 1;
+				uint8_t tf : 1;
+				uint8_t ief : 1; /* if */
+				uint8_t df : 1;
+				uint8_t of : 1;
+				uint8_t iopl : 1;
+				uint8_t nt : 1;
+				uint8_t __reserved15 : 1;
+				uint8_t rf : 1;
+				uint8_t vm : 1;
+				uint8_t ac : 1;
+				uint8_t vif : 1;
+				uint8_t vip : 1;
+				uint8_t id : 1;
+			};
+		};
+	};
+	// EIP
+	swaddr_t eip;
 } CPU_state;
 
 extern CPU_state cpu;
