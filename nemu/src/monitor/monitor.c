2ed5837ff3901f9364cb760865f4453ce185d3ac
swordfeng <swordfeng123@gmail.com>
2015-10-15 11:22:43 +0800

    no need to init regex

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -32,9 +32,6 @@ void init_monitor(int argc, char *argv[]) {
 	/* Load the string table and symbol table from the ELF file for future use. */
 	load_elf_tables(argc, argv);
 
-	/* Compile the regular expressions. */
-	init_regex();
-
 	/* Initialize the watchpoint link list. */
 	init_wp_list();
 
