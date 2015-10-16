07df860c9552f5e372e1088f57cc94df8f46a7f3
swordfeng <swordfeng123@gmail.com>
2015-10-16 10:46:46 +0800

    fix sub bug / add cmp instruction

--- nemu/src/cpu/arith/cmp.h
+++ nemu/src/cpu/arith/cmp.h
@@ -4,15 +4,15 @@ TEMPLATE_INSTRUCTION_HELPER(sub) {
     uint32_t val2 = ctx.operands[1].getSignedValue();
     size_t result_size = ctx.operands[0].size;
     uint32_t result = signed_extend(int_trunc(val1 - val2, result_size), result_size);
-    ctx.operands[0].setValue(result);
+
     cpu.cf = int_trunc(result, result_size) > int_trunc(val1, result_size);
     cpu.zf = result == 0;
     cpu.sf = result >> 31;
     cpu.of = (val1 >> 31) != (val2 >> 31) && (val1 >> 31) != (result >> 31);
     cpu.pf = calc_pf(result);
     // AF ignored
-    string instr_name = "sub";
-    if (ctx.operands[0].type == opt_address && ctx.operands[1].type != opt_register) {
+    string instr_name = "cmp";
+    if (ctx.operands[0].type != opt_register && ctx.operands[1].type != opt_register) {
         instr_name += ctx.operands[0].suffix();
     }
     print_instr(ctx, instr_name);
