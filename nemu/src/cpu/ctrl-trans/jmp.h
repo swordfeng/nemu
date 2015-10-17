4c30c8d576d34b3f59c5d0fbf65f7a5f19ba39bd
swordfeng <swordfeng123@gmail.com>
2015-10-17 20:47:32 +0800

    add jmp

---
+++ nemu/src/cpu/ctrl-trans/jmp.h
@@ -0,0 +1,17 @@
+TEMPLATE_INSTRUCTION_HELPER(jump_near) {
+    int len = decode_operands(ctx, eip);
+    uint32_t temp_eip = 0;
+    if (op_name_is(op, imm)) {
+        // rel8 / rel16 / rel32 relative address
+        temp_eip = eip + len + ctx.operands[0].getSignedValue();
+    } else {
+        // r/m absolute address
+        temp_eip = ctx.operands[0].getUnsignedValue();
+    }
+    if (ctx.operands[0].size == 16) temp_eip &= 0xffff;
+
+    print_asm("jmp\t%x", temp_eip);
+
+    // eip <- temp_eip;
+    return temp_eip - eip;
+}
