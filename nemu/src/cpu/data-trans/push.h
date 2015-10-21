ad791f83744a7de39de40c09b0e833c622aa3908
swordfeng <swordfeng123@gmail.com>
2015-10-21 15:38:49 +0800

    fix push

--- nemu/src/cpu/data-trans/push.h
+++ nemu/src/cpu/data-trans/push.h
@@ -1,7 +1,7 @@
 TEMPLATE_INSTRUCTION_HELPER(push) {
   // NOT IMPLEMENTED: Detection of Address size, Stack-address Size.
   // See manual.
-  size_t data_size = ctx.prefix[PREFIX_DATA] ? 2 : 4;
+  size_t data_size = ctx.prefix[prefix_operand] ? 2 : 4;
   uint32_t val = ctx.operands[0].getSignedValue();
   reg_l(R_ESP) -= data_size;
   swaddr_write(reg_l(R_ESP), data_size, val);
