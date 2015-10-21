db59e5719a23620613b400809e6aea019d11c64e
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:41:49 +0800

    >  run 14307130245 swordfeng Linux swordfeng-PC 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  21:41:49 up  3:00,  5 users,  load average: 0.02, 0.08, 0.12 caf77079a201114299404693bcac1c95a24a75db

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -17,8 +17,8 @@ int main() {
 	FLOAT x1_ans = f2F(0.618);
 	FLOAT x2_ans = f2F(-1.618);
 
-	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
-	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
+	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-3));
+	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-3));
 
 	HIT_GOOD_TRAP;
 	return 0;
