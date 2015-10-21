8b457cb8978e4f00092479ea0783915007cd59d8
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:50:14 +0800

    >  run 14307130245 swordfeng Linux swordfeng-PC 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  21:50:14 up  3:09,  5 users,  load average: 0.13, 0.08, 0.11 d64c9d06294c9ee82d301cd7f3ba3fbae83bb865

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -9,7 +9,7 @@ int main() {
 	FLOAT c = f2F(-0.5);
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
-	nemu_assert(F_mul_F(F_mul_int(a, 4), c) == 0x00004000);
+	nemu_assert(F_mul_F(F_mul_int(a, 4), c) == 0xffff0000);
 	nemu_assert(dt == 0x00014000);
 	FLOAT sqrt_dt = sqrt(dt);
 
