f0a4e3e2847bbc1d327a767ea717033e01d46ee9
tracer-ics2015 <tracer@njuics.org>
2015-10-23 11:04:00 +0800

    >  run 14307130245 swordfeng Linux swordfeng-PC 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  11:04:00 up  1:50,  4 users,  load average: 0.00, 0.03, 0.08 7d5dbbb35c61d4e11f27709750b03f6ee79b001c

--- testcase/src/sub-longlong-simple.c
+++ testcase/src/sub-longlong-simple.c
@@ -1,7 +1,7 @@
 #include "trap.h"
 
 int main() {
-	nemu_assert(0LL - 1LL == 0xffffffffffffffffULL);
+	nemu_assert((long long)0 - (long long)1 == 0xffffffffffffffffULL);
 	HIT_GOOD_TRAP;
 	return 0;
 }
