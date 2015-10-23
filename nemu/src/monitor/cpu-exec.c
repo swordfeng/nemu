25689ac48cfce5610d1e5d54616ccd47015a28d2
swordfeng <swordfeng123@gmail.com>
2015-10-23 16:23:01 +0800

    do not write log when exec instr now

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -73,7 +73,7 @@ void cpu_exec(volatile uint32_t n) {
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
-		Log_write("%s\n", asm_buf);
+		//Log_write("%s\n", asm_buf);
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
