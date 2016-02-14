6c5866e886dffc92ea8281574023c11514503ca1
Jianfeng Zhang <swordfeng123@gmail.com>
2016-02-14 19:36:04 +0800

    port sdlpal

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -24,12 +24,10 @@ timer_event(void) {
 }
 
 uint32_t SDL_GetTicks() {
-	/* TODO: Return the time in millisecond. */
-	assert(0);
-	return 0;
+	return jiffy * 10;
 }
 
 void SDL_Delay(uint32_t ms) {
-	/* TODO: Return from this function after waiting for `ms' milliseconds. */
-	assert(0);
+    uint32_t startTime = SDL_GetTicks();
+    while (SDL_GetTicks() < startTime + ms);
 }
