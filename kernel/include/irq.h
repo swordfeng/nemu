56787f65c2242877d60faa57afe4d698a36f2222
Jianfeng Zhang <swordfeng123@gmail.com>
2016-02-14 18:27:58 +0800

    fix elf reading bug

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -3,11 +3,6 @@
 
 #include "common.h"
 
-/* TODO: The decleration order of the members in the ``TrapFrame''
- * structure below is wrong. Please re-orgainize it for the C
- * code to use the trap frame correctly.
- */
-
 typedef struct TrapFrame {
 	uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax;
 	int32_t irq;
