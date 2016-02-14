d6970d6779e48743e11f04d5663770fa5fb03b1b
Jianfeng Zhang <swordfeng123@gmail.com>
2016-02-14 11:04:35 +0800

    fix popa bug

--- nemu/src/cpu/data-trans/popa.h
+++ nemu/src/cpu/data-trans/popa.h
@@ -2,12 +2,12 @@ INSTRUCTION_HELPER(popa) {
     size_t sz = ctx.prefix[prefix_operand] ? 2 : 4;
     uint32_t temp = cpu.esp;
     if (sz == 2) {
-        for (size_t index = 8; index + 1 != 0; index--) {
+        for (size_t index = 7; index + 1 != 0; index--) {
             cpu.gpr[index]._16 = swaddr_read(temp, sz, sreg_index(ss));
             temp += sz;
         }
     } else {
-        for (size_t index = 8; index + 1 != 0; index--) {
+        for (size_t index = 7; index + 1 != 0; index--) {
             cpu.gpr[index]._32 = swaddr_read(temp, sz, sreg_index(ss));
             temp += sz;
         }
