c925a0794f390cc8d2bff7e92290d9ea5c7154a5
swordfeng <swordfeng123@gmail.com>
2016-03-01 11:10:57 +0800

    remote rust testcases

--- lib-common/core.rs
+++ lib-common/core.rs
@@ -5,6 +5,6 @@
 // These functions and traits are used by the compiler, but not
 // for a bare-bones hello world. These are normally
 // provided by libstd.
-//#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
