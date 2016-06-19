4c901b347a7a18e02d2592ffac259b06a45093cc
Jianfeng Zhang <swordfeng123@gmail.com>
2016-06-20 00:24:43 +0800

    fix keyboard finally

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -4,7 +4,7 @@
 #define NR_KEYS 18
 #define I8042_DATA_PORT 0x60
 
-enum {KEY_STATE_RELEASED, KEY_STATE_PRESSED, KEY_STATE_PENDING_RELEASE, KEY_STATE_PRESS};
+enum {KEY_STATE_RELEASED, KEY_STATE_PRESS, KEY_STATE_PRESSED, KEY_STATE_PRESS_RELEASE, KEY_STATE_RELEASE};
 
 /* Only the following keys are used in NEMU-PAL. */
 static const int keycode_array[] = {
@@ -29,11 +29,18 @@ keyboard_event(void) {
         }
     }
     if (index == -1) return;
-    if (release) return;
-    if (key_state[index] == KEY_STATE_RELEASED) {
-        key_state[index] = KEY_STATE_PRESS;
-    } else if (key_state[index] == KEY_STATE_PENDING_RELEASE) {
-        key_state[index] = KEY_STATE_PRESSED;
+    if (!release) {
+        if (key_state[index] == KEY_STATE_RELEASE || key_state[index] == KEY_STATE_PRESSED) {
+            key_state[index] = KEY_STATE_PRESSED;
+        } else {
+            key_state[index] = KEY_STATE_PRESS;
+        }
+    } else {
+        if (key_state[index] == KEY_STATE_PRESS) {
+            key_state[index] = KEY_STATE_PRESS_RELEASE;
+        } else if (key_state[index] == KEY_STATE_PRESSED) {
+            key_state[index] = KEY_STATE_RELEASE;
+        } else panic("unexpected key state!");
     }
 }
 
@@ -43,12 +50,13 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
     for (int index = 0; index < NR_KEYS; index++) {
         if (key_state[index] == KEY_STATE_PRESS) {
             key_press_callback(keycode_array[index]);
-            key_state[index] = KEY_STATE_PENDING_RELEASE;
-        } else if (key_state[index] == KEY_STATE_PENDING_RELEASE) {
+            key_state[index] = KEY_STATE_PRESSED;
+        } else if (key_state[index] == KEY_STATE_PRESS_RELEASE) {
+            key_press_callback(keycode_array[index]);
+            key_state[index] = KEY_STATE_RELEASE;
+        } else if (key_state[index] == KEY_STATE_RELEASE) {
             key_release_callback(keycode_array[index]);
             key_state[index] = KEY_STATE_RELEASED;
-        } else if (key_state[index] == KEY_STATE_PRESSED) {
-            key_state[index] = KEY_STATE_PENDING_RELEASE;
         }
     }
     sti();
